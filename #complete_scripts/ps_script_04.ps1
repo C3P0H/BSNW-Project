092a6c74e745817f9bdac15ef869b323f2353b99
C3P0H <marvineckerc13@gmail.com>
2014-11-30 20:51:51 +0100

    Initial commit

---
+++ #complete_scripts/ps_script_04.ps1
@@ -0,0 +1,31 @@
+﻿#-- Version History --
+#Version:         01.000           
+#Creation:        30.11.2014 
+#Author:          Marvin Ecker
+#Description:     The following script saves all processes into a .txt file.
+
+#-- Version Changes --
+#30.11.2014:      Inital commit.
+
+#-- Code Section --
+
+$Global:destinationFile = "C:\Users\Marvin\Desktop\dienste.txt";
+
+Function CheckFile ()
+{
+    if((Test-Path $Global:destinationFile) -eq 1){
+        Remove-Item $Global:destinationFile -Force;
+        Write-Host "File has been deleted!"
+    }
+}
+
+Function SaveProcess ()
+{
+    Get-Process | Out-File $Global:destinationFile;
+    Write-Host "Processes have been saved!"
+}
+
+CheckFile;
+SaveProcess;
+
+#-- End of application --
---
+++ #incomplete_scripts/ps_script_02.ps1
@@ -0,0 +1,38 @@
+﻿#-- Version History --
+#Version:         01.000           
+#Creation:        30.11.2014 
+#Author:          Marvin Ecker
+#Description:     The following script provides a function to backup directorys.
+
+#-- Version Changes --
+#30.11.2014:      Inital commit.
+
+#-- Code Section --
+
+#-- ToDo --
+#Check ArchiveBit
+
+$Global:backupDir = "C:/Users/Marvin/Backup";
+$Global:sourceDir = "C:/Users/Marvin/Documents";
+
+Function CopyItems ()
+{
+        Copy-Item -Recurse $Global:sourceDir -Destination $Global:backupDir -Force;
+        Write-Host "Backup was sucessfull!"
+}
+
+Function CreateBackup ()
+{
+    if((Test-Path $Global:backupDir) -eq 0){
+        New-Item -ItemType Directory -Force -Path $Global:backupDir -ErrorAction Stop;
+        CopyItems;
+    }
+
+    if((Test-Path $Global:backupDir) -eq 1){
+        CopyItems;
+    }
+}
+
+CreateBackup;
+
+#-- End of application --
