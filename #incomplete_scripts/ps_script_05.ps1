bced228d75a694eb54deaf244edf1352ad3ef673
C3P0H <marvineckerc13@gmail.com>
2014-12-01 21:03:52 +0100

    exercise_5 has been added

---
+++ #incomplete_scripts/ps_script_05.ps1
@@ -0,0 +1,22 @@
+﻿#-- Version History --
+#Version:         01.000           
+#Creation:        01.12.2014 
+#Author:          Marvin Ecker
+#Description:     The following script clears the temp folders.
+
+#-- Version Changes --
+#01.12.2014:      initial commit
+
+#-- Code Section --
+
+#ToDo
+#Script should be able to interact in a network.
+
+$tempfolders = @( "C:\Windows\Temp\*", "C:\Windows\Prefetch\*", "C:\Users\*\Appdata\Local\Temp\*");
+
+foreach($item in $tempfolders)
+{
+        Remove-Item $item -force -recurse;
+}
+
+#-- End of application --
