bd5ee050cb44e91fdafebf1141e2dd91e166deea
LuZziD <ziebartjevgeni@googlemail.com>
2014-12-04 11:43:00 +0100

    exercise_8 revised

--- #incomplete_scripts/ps_script_08.ps1
+++ #incomplete_scripts/ps_script_08.ps1
@@ -2,66 +2,85 @@
 #Version:         01.000           
 #Creation:        01.12.2014 
 #Author:          Jevgeni Ziebart
-#Description:     The following script provides a complete system shutdown.
+#Description:     The following script copies all files from a USB-Flashdrive to your harddisk.
 
 #-- Version Changes --
-#01.12.2014:      Done some fancy shit(itsabeta).
+#01.12.2014:      initial commit
 
 #-- Code Section --
 
 #ToDo
-#replace that workaround sparta usbDir function with something "more suitable" 
-#(i dont need you to see that its shit marvin)
-#removing double output after successfull copying
+#Adding support for 2 or more USB-Flashdrives
 
 
-$Global:hddDir = "c:\usb";
-$Global:usbDir = "";
-$Global:userUsbDir = "d";
+$Global:usbDir;
+$Global:hddDir;
 
+Function ClearVariables()
+{
+    $Global:usbDir = "";
+    $Global:hddDir = "";
+    $dLetter = "";
+}
 
 Function CopyFiles()
 {
     Copy-Item $Global:usbDir $Global:hddDir -Recurse -Force;
-    echo "Your files were successfully copied to $hddDir";
+    Write-Host "Your files were successfully copied to $hddDir";
     DeleteFiles;
 }
 
 Function CheckDirs()
 {
+    $Global:hddDir = Read-Host "Where do you want to save your flashdrive files?";
     if((Test-Path -Path $Global:hddDir) -eq 0)
     {
-    New-Item $Global:hddDir -type directory;
-    Copyfiles;
+        New-Item $Global:hddDir -type directory;
+        Copyfiles;
     }
-
-    if((Test-Path -Path $Global:hddDir) -eq 1)
+    else
     {
-    CopyFiles;
+        CopyFiles;
     }
 }
 
 Function DeleteFiles()
 {
     $delete = Read-Host "Do you want to delete any file on your flashdrive? y/n";
-    if(($delete) -eq "n")
-    {
-    exit;
-    }
-
     if(($delete) -eq "y")
     {
-    Remove-Item -Recurse -path $Global:usbDir;
-    echo "Your files were deleted";
+        Remove-Item -Recurse -path $Global:usbDir;
+        Write-Host "Your files were deleted";
+    }
+    else
+    {
+        ClearVariables;
+        Exit;
     }
 }
 
-Function Sparta()
-{
-    $Global:userUsbDir= Read-Host "What driveletter is your flashdrive";
-    $Global:usbDir=$Global:userUsbDir+":\*";
-    CheckDirs;
+Function AutoSelect()
+{   
+    try
+    {
+        $dLetter = gwmi win32_diskdrive | ?{$_.interfacetype -eq "USB"} | %{gwmi -Query "ASSOCIATORS OF {Win32_DiskDrive.DeviceID=`"$($_.DeviceID.replace('\','\\'))`"} WHERE AssocClass = Win32_DiskDriveToDiskPartition"} |  %{gwmi -Query "ASSOCIATORS OF {Win32_DiskPartition.DeviceID=`"$($_.DeviceID)`"} WHERE AssocClass = Win32_LogicalDiskToPartition"} | %{$_.deviceid};
+        $Global:usbDir = $dLetter.TrimEnd(" ")+"\*";
+        if(($Global:usbDir))
+        {
+
+        }
+        CheckDirs;
+
+    }
+    catch
+    {
+        Write-Host "No USB-Flashdrive is connected to the computer";
+        ClearVariables;
+        Exit;
+    }
+
 }
-Sparta;
+
+AutoSelect;
 
 #-- End of application --
