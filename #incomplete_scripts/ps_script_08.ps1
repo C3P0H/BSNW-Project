c4a713e263d78fc711d7ac3662977e32eea1564c
LuZziD <ziebartjevgeni@googlemail.com>
2014-12-14 16:45:52 +0100

    exercise 8 finished

--- #incomplete_scripts/ps_script_08.ps1
+++ #incomplete_scripts/ps_script_08.ps1
@@ -10,77 +10,114 @@
 #-- Code Section --
 
 #ToDo
-#Adding support for 2 or more USB-Flashdrives
+#
 
 
-$Global:usbDir;
-$Global:hddDir;
+$Global:usb_dir;
+$Global:hdd_dir;
 
-Function ClearVariables()
+Function copy_files()
 {
-    $Global:usbDir = "";
-    $Global:hddDir = "";
-    $dLetter = "";
-}
-
-Function CopyFiles()
-{
-    Copy-Item $Global:usbDir $Global:hddDir -Recurse -Force;
-    Write-Host "Your files were successfully copied to $hddDir";
-    DeleteFiles;
-}
-
-Function CheckDirs()
-{
-    $Global:hddDir = Read-Host "Where do you want to save your flashdrive files?";
-    if((Test-Path -Path $Global:hddDir) -eq 0)
+    try
     {
-        New-Item $Global:hddDir -type directory;
-        Copyfiles;
+        Copy-Item $Global:usb_dir $Global:hdd_Dir -Recurse -Force;
+        echo "Your files were successfully copied to $hdd_dir";
+        delete_files;
     }
-    else
+    catch
     {
-        CopyFiles;
+        echo "File Copy Error, restart?"
+        Pause;
+        copy_files;
     }
 }
 
-Function DeleteFiles()
+Function check_dirs()
+{
+    $Global:hdd_dir = Read-Host "Where do you want to save your flashdrive files?";
+    try
+    {
+        if((Test-Path -Path $Global:hdd_dir) -eq 0)
+        {
+            New-Item $Global:hdd_dir -type directory;
+            copy_files;
+        }
+        else
+        {
+            copy_files;
+        }
+    }
+    catch
+    {
+        echo "Folder couldn't be created, restart?"
+        Pause;
+        check_dirs;
+    }
+}
+
+Function delete_files()
 {
     $delete = Read-Host "Do you want to delete any file on your flashdrive? y/n";
-    if(($delete) -eq "y")
+    try
     {
-        Remove-Item -Recurse -path $Global:usbDir;
-        Write-Host "Your files were deleted";
+        if(($delete) -eq "y")
+        {
+            Remove-Item -Recurse -path $Global:usb_dir;
+            echo "Your files were deleted";
+        }
+        else
+        {
+            Exit;
+        }
     }
-    else
+    catch
     {
-        ClearVariables;
-        Exit;
+        echo "Your files couldn't be deleted, restart?";
+        Pause;
+        delete_files;    
     }
 }
 
-Function AutoSelect()
+Function auto_select()
 {   
     try
     {
-        $dLetter = gwmi win32_diskdrive | ?{$_.interfacetype -eq "USB"} | %{gwmi -Query "ASSOCIATORS OF {Win32_DiskDrive.DeviceID=`"$($_.DeviceID.replace('\','\\'))`"} WHERE AssocClass = Win32_DiskDriveToDiskPartition"} |  %{gwmi -Query "ASSOCIATORS OF {Win32_DiskPartition.DeviceID=`"$($_.DeviceID)`"} WHERE AssocClass = Win32_LogicalDiskToPartition"} | %{$_.deviceid};
-        $Global:usbDir = $dLetter.TrimEnd(" ")+"\*";
-        if(($Global:usbDir))
+        $d_letter = gwmi win32_diskdrive | ?{$_.interfacetype -eq "USB"} | %{gwmi -Query "ASSOCIATORS OF {Win32_DiskDrive.DeviceID=`"$($_.DeviceID.replace('\','\\'))`"} WHERE AssocClass = Win32_DiskDriveToDiskPartition"} |  %{gwmi -Query "ASSOCIATORS OF {Win32_DiskPartition.DeviceID=`"$($_.DeviceID)`"} WHERE AssocClass = Win32_LogicalDiskToPartition"} | %{$_.deviceid};
+        $d_letter = $d_letter.Replace(" ","").Replace(":","");
+        if(($d_letter.Length) -eq 1)
         {
-
+            echo "Flashdrive Driveletter: "$d_letter;
+            check_dirs;
         }
-        CheckDirs;
+        else
+        {
+            echo "You have connected more than 1 USB-Flashdrive to your PC, please select the USB-Flashdrive Driveletter you want to copy your files from."
+            echo $d_letter;
+            $user_letter = Read-Host;
+            if($d_letter.contains($user_letter))
+            {
+                $Global:usb_dir = $user_letter + ":\*";
+                check_dirs;
+            }
+            else
+            {
+                echo "You have selected a Driveletter to an non existing file. Please select a Driveletter to an existing Device";
+                Pause;
+                auto_select;
+            }
+        }
 
     }
     catch
     {
-        Write-Host "No USB-Flashdrive is connected to the computer";
-        ClearVariables;
-        Exit;
+        echo "No USB-Flashdrive is connected to the computer";
+        echo "Please connect a USB-Flashdrive to the computer";
+        Pause;
+        auto_select;
     }
 
 }
 
-AutoSelect;
+auto_select;
 
 #-- End of application --
