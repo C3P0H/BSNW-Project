994bf5307a6364d74977653478a8bb29d8a1f4f0
Kyrem <Lars.Wrzeziono@t-online.de>
2014-12-14 19:38:28 +0100

    excercise_9 revised

--- #incomplete_scripts/ps_script_09.ps1
+++ #incomplete_scripts/ps_script_09.ps1
@@ -9,13 +9,18 @@
 
 #-- Code Section --
 
-$drive = Read-Host "Drive"
-$pc_number = Read-Host "PC-Number"
-$type = Read-Host "Type"
-$room_number = Read-Host "room_number"
+# Getting the Folders and delete all the Files
+Function DeleteFiles ()
+{
+$Global:Drive = Read-Host "Drive"
+$Global:Pc_number = Read-Host "PC-Number"
+$Global:Type = Read-Host "Type"
+$Globa:Room_number = Read-Host "room_number"
 
-Join-Path $drive "$pc_number$type$room_number"
-Remove-Item $drive -Force -Recurse -WhatIf
-echo $path
+$Global:Drive = Join-Path $Global:Drive "$Global:Pc_number$Global:Type$Global:Room_number"
+cd $Global:Drive
+Remove-Item $Global:Drive -Force -Recurse -WhatIf
+}
+DeleteFiles;
 
 #-- End of application --
