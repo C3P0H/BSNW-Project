e790c3a1c65ffcaec7eba0897d74b088b707dba2
C3P0H <marvineckerc13@gmail.com>
2014-12-26 21:20:21 +0100

    Script04.ps1 has been added

---
+++ #complete_scripts/script04.ps1
@@ -0,0 +1,27 @@
+﻿<#
+.Synopsis
+   The following script saves all services
+.DESCRIPTION
+   the following script saves all currently running services into a file
+.PARAMETER ComputerName
+   Enter the machine
+.PARAMETER FilePath
+   Enter the save path of the file
+.EXAMPLE
+   script_04 -ComputerName Server_01 Server_04 -FilePath C:\Logs
+#>
+
+<#
+Author: Marvin Ecker
+#>
+
+
+[CmdletBinding()]
+Param
+(
+    [Parameter(Mandatory=$true)]
+    [String[]]$ComputerName,
+    [String]$FilePath
+)
+
+Get-Service -ComputerName $ComputerName | Out-File -FilePath $FilePath 
