90d27a70f1d5db1ecc291f861bb93c85eecd5935
C3P0H <marvineckerc13@gmail.com>
2014-12-26 21:19:15 +0100

    Script01.ps1 has been added

---
+++ #complete_scripts/script01.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.Synopsis
+   The following script executes a system shutdown
+.DESCRIPTION
+   the following script executes a system shutdown, based on a cmdlet
+.PARAMETER ComputerName
+   Enter the machine, which you want to shutdown
+.EXAMPLE
+   ps_script_01 -ComputerName Server_01
+#>
+
+<#
+Author: Marvin Ecker
+#>
+
+
+[CmdletBinding()]
+Param
+(
+    [Parameter(Mandatory=$true)]
+    [String[]]$ComputerName
+)
+
+Stop-Computer -ComputerName $ComputerName -Confirm
