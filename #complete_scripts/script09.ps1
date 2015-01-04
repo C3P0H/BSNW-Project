e62e518880a58d70a3b33e1ff543c7c971cd3c8f
Kyrem <Lars.Wrzeziono@t-online.de>
2015-01-04 14:37:58 +0100

    Script09.ps1 added

---
+++ #complete_scripts/script09.ps1
@@ -0,0 +1,42 @@
+<#
+.Synopsis
+    The following script provides a complete clear of a Network folder.
+.DESCRIPTION
+    The following script provides a complete clear of a Network folder.
+.PARAMETER Drive
+    Enter the Drive Letter
+.PARAMETER PC-Number
+    Enter the PC-Number
+.PARAMETER Type
+    Enter the Type
+.PARAMETER Room-Number
+    Enter the Room Number
+.EXAMPLE script09 -Drive Z: -PC-Number PC01 -Type KA -Room-Number 121
+#>
+
+<#
+Author: Lars Wrzeziono
+#>
+
+[CmdletBinding()]
+Param
+(
+    [Parameter (Mandatory=$true)]
+    [String]$Drive,
+    [String]$PC_Number,
+    [String]$Type,
+    [String]$Room_Number
+)
+
+Function DeleteFiles ()
+{
+$Pc_number = Read-Host "PC-Number"
+$Type = Read-Host "Type"
+$Room_number = Read-Host "room_number"
+$Drive = Join-Path $Drive "$Pc_Number$Type$Room_Number\*"
+echo $Drive
+Remove-Item  $Drive -Force -Recurse
+}
+
+#entry point
+DeleteFiles;
