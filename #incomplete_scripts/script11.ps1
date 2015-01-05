cb330c77716e2a53fd659699f9fe07c499087405
LuZziD <ziebartjevgeni@googlemail.com>
2015-01-05 17:18:58 +0100

    script11.ps1 added

--- #incomplete_scripts/script11.ps1
+++ #incomplete_scripts/script11.ps1
@@ -6,9 +6,16 @@
 .PARAMETER 
    
 .EXAMPLE
-   script11.ps1
+   .\script11.ps1
 #>
 
 <#
 Author: Jevgeni Ziebart
 #>
+Function CreatePartition
+{
+    
+    Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle GPT -PassThru |New-Partition -AssignDriveLetter -Size 10GB |Format-Volume -FileSystem NTFS
+}
+#entry point
+CreatePartition
